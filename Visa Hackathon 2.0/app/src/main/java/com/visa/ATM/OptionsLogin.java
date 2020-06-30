c02d02562aa5d3100cb392764cde5bb1bccce42c
dhruvagja8808 <dhruvagja8808@gmail.com>
2020-06-30 11:32:54 +0530

    OptionsLogin Ui #1

--- Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/OptionsLogin.java
+++ Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/OptionsLogin.java
@@ -37,7 +37,7 @@ public class OptionsLogin extends AppCompatActivity {
     }
 
     private void init(){
-        cashProvider = this.findViewById(R.id.b_cashProvide);
+        cashProvider = this.findViewById(R.id.b_cashProvider);
         user = this.findViewById(R.id.b_user);
     }
 }
