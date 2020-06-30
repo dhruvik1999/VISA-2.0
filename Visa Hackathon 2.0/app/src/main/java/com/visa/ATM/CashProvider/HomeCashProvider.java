8066fbc5465e99c13fb7137cbc89defa43d68fe9
Aashay123 <aashaym1@gmail.com>
2020-06-30 11:35:37 +0530

    Splash Screen

--- Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/CashProvider/HomeCashProvider.java
+++ Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/CashProvider/HomeCashProvider.java
@@ -37,6 +37,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.visa.ATM.R;
 import com.visa.ATM.Request;
 import com.visa.ATM.data;
@@ -58,7 +64,23 @@ public class HomeCashProvider extends  AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_cash_provider);
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                    Toast.makeText(HomeCashProvider.this,"You Accepted Permission !",Toast.LENGTH_SHORT).show();
+                    }
 
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(HomeCashProvider.this,"You Sholud Accept This Permission !!",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
         RecyclerView recycler = findViewById(R.id.recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(layoutManager);
