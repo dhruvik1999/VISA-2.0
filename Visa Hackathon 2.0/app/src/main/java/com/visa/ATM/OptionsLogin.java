27ea4107ca9a5ce0c5df0610f75b622c9a2f387d
Aashay123 <aashaym1@gmail.com>
2020-06-30 12:52:51 +0530

    ui

--- Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/OptionsLogin.java
+++ Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/OptionsLogin.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
