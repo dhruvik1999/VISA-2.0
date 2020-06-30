55ebdd6110dfc1869191e88725c4fe1ec1caa841
Aashay123 <aashaym1@gmail.com>
2020-06-30 11:35:37 +0530

    Splash Screen

--- Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/MainActivity.java
+++ Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,26 +17,32 @@ import com.google.firebase.database.ValueEventListener;
 import com.visa.ATM.User.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN_TIME_OUT=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
         setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
 
-//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("check");
-//        reference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Toast.makeText(getApplicationContext(),dataSnapshot.getValue().toString(),Toast.LENGTH_LONG).show();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        OptionsLogin.class);
+                //Intent is used to switch from one activity to another.
 
-        startActivity(new Intent(this, OptionsLogin.class));
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
