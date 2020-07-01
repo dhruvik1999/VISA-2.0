b933cbfe228ca9e2547444e829ab233e59b39205
dhruvik1999 <navadiyadhruvik2@gmail.com>
2020-07-01 10:54:43 +0530

    qr code scanner init

---
+++ Visa Hackathon 2.0/app/src/main/java/com/visa/ATM/QrCodeHelper/QrCodeScanner.java
@@ -0,0 +1,16 @@
+package com.visa.ATM.QrCodeHelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.visa.ATM.R;
+
+public class QrCodeScanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_scanner);
+    }
+}
